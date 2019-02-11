5616d62685230272ce0e3eb5de9b17ee7b35b848
Alan Campbell <=>
2019-02-11 23:43:17 +0000

    Removed

--- Package.swift
+++ Package.swift
@@ -2,32 +2,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "DrawTheJoker",
-    products: [
-        .library(name: "App", targets: ["App"]),
-        .executable(name: "Run", targets: ["Run"])
-    ],
+    name: "blockchain-web-api",
     dependencies: [
         // 💧 A server-side Swift web framework.
-       // .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-      //  .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
     ],
-//    targets: [
-//        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
-//        .target(name: "Run", dependencies: ["App"]),
-//        .testTarget(name: "AppTests", dependencies: ["App"])
-//    ]
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentProvider"],
-                exclude: [
-                    "Config",
-                    "Public",
-                    "Resources",
-                    ]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+        .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
+
