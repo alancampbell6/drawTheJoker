6abdee5c4dfda84e17e3808101403bb35fb667bc
Alan Campbell <=>
2019-02-11 22:46:21 +0000

    Test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works! Bitch"
+        return "It works! XXX"
     }
     
     // Basic "Hello, world!" example
